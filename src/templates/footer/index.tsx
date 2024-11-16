30eda5debb9fb524750401735bfeddc3438ee183
TakedaRyota <tkdryt05@gmail.com>
2024-11-16 18:18:18 +0900

    スケジュール追加

--- src/templates/footer/index.tsx
+++ src/templates/footer/index.tsx
@@ -1,5 +1,5 @@
 export const Footer = () => {
   return (
-    <div className='w-full py-4 px-6 text-center'>Copyright © aiMe official 2024 All Rights Reserved.</div>
+    <div className='w-full py-4 px-6 text-center'>Copyright © aiMe All Rights Reserved.</div>
   )
 }
--- src/views/schedule/index.tsx
+++ src/views/schedule/index.tsx
@@ -7,7 +7,12 @@ export const ScheduleView = () => {
       <div className='w-full py-16 px-8'>
         <Container maxWidth='lg'>
           <div className='text-center font-mono text-[48px] py-8'>SCHEDULE</div>
-          <div className='flex flex-col gap-4'></div>
+          <div className='flex flex-col gap-4'>
+            <iframe
+              title='カレンダー'
+              src='https://timetreeapp.com/public_calendars/aimestaff/embed?calendar_name=false&frame_color=%23c2e8ff'
+              className='w-full border-none h-[700px]' />
+          </div>
         </Container>
       </div>
     </BaseViewLayout>
