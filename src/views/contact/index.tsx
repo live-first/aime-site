86d140e848ff665feb3ec8132f42a11e3ecf78a6
TakedaRyota <tkdryt05@gmail.com>
2024-11-17 10:27:02 +0900

    contactページの作成

--- src/views/contact/index.tsx
+++ src/views/contact/index.tsx
@@ -1,3 +1,15 @@
+import { Container } from '@mui/material'
+import { BaseViewLayout } from '../../layouts/BaseViewLayout'
+
 export const ContactView = () => {
-  return <></>
+  return (
+    <BaseViewLayout>
+      <div className='w-full py-16 px-8 flex flex-col gap-6'>
+        <Container maxWidth='lg'>
+          <div className='text-center font-mono text-[48px] py-8'>CONTACT</div>
+          <div className='flex flex-col gap-4'></div>
+        </Container>
+      </div>
+    </BaseViewLayout>
+  )
 }
